60e010afcfdc048cb2dbb4073c618813dbbd8984
Aidan Feldman <aidan.feldman@gmail.com>
2012-07-13 17:35:36 -0300

    fix loading of image - as only happening the first time

--- lib/facial.rb
+++ lib/facial.rb
@@ -6,12 +6,10 @@ include OpenCV
 module Facial
   class << self
     def detect image_path
-      unless @detector
-        # initialize the detector
-        image = IplImage::load image_path
-        @detector = CvHaarClassifierCascade::load(File.expand_path(File.dirname(__FILE__) + '/../data/haarcascade_frontalface_alt.xml'))
-      end
+      # initialize the detector, if it wasn't already
+      @detector ||= CvHaarClassifierCascade::load(File.expand_path(File.dirname(__FILE__) + '/../data/haarcascade_frontalface_alt.xml'))
       
+      image = IplImage::load image_path
       @detector.detect_objects(image).map do |rect|
         {
           top_left: rect.top_left,
